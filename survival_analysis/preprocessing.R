e37141c09002b7a0bae3d0b9d807d6eae471ed27
RIYA ELIZA <riyaelizashaju@gmail.com>
2024-05-21 10:27:12 -0700

    Creating preprocessing files for data

---
+++ survival_analysis/preprocessing.R
@@ -0,0 +1,8 @@
+library(readr)
+
+hatchery <- read_csv("raw_data/hatchery.csv")
+downstream <- read_csv("raw_data/downstream.csv")
+microtroll_hatch <- read_csv("raw_data/microtroll_hatch.csv")
+microtroll_wild <- read_csv("raw_data/microtroll_wild.csv")
+
+
