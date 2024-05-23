c0b5b08c3c37cd6d929029f2218e0f28ad76e51b
RIYA ELIZA <riyaelizashaju@gmail.com>
2024-05-23 13:13:11 -0700

    Preprocessing.R complete, small dataset ready for model creation and validation

--- survival_analysis/preprocessing.R
+++ survival_analysis/preprocessing.R
@@ -1,8 +1,16 @@
 library(readr)
 
 hatchery <- read_csv("raw_data/hatchery.csv")
-downstream <- read_csv("raw_data/downstream.csv")
+downstream_hatch <- read_csv("raw_data/downstream_hatch.csv")
+downstream_wild <- read_csv("raw_data/downstream_wild.csv")
+estuary <- read_csv("raw_data/estuary.csv")
 microtroll_hatch <- read_csv("raw_data/microtroll_hatch.csv")
 microtroll_wild <- read_csv("raw_data/microtroll_wild.csv")
+return <- read_csv("raw_data/return.csv")
 
+combined <- rbind(hatchery, downstream_hatch, downstream_wild, estuary,
+                  microtroll_hatch, microtroll_wild, return)
 
+combined$action[combined$action == 'recap'] <- 'detect'
+
+write.csv(combined, "../survival_analysis/preprocessed_data/survival_analysis.csv")
