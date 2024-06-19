b7280acda8ebe9e82a0cec3043975321cd3b66b1
Rafe Chang <cchang5@student.ubc.ca>
2024-06-19 11:56:04 -0700

    inProgress

--- outMigration_model/scripts/outMigration_preprocessing.py
+++ outMigration_model/scripts/outMigration_preprocessing.py
@@ -14,6 +14,8 @@ GROUP BY DATE(min_dt.date_time), min_dt.species
 ORDER BY DATE(min_dt.date_time);
 """
 
+cowichan_historic = pd.DataFrame(plpy.execute(cowichan_historic_query))
+
 salmon_query = """
 SELECT DATE(MIN(f.date)) AS date, f.watershed, f.site, f.species, COUNT(DISTINCT f.tag_id_long) AS count
 FROM FIELD f
@@ -25,6 +27,8 @@ INNER JOIN (
 GROUP BY f.watershed, f.site, f.species, DATE(f.date)
 ORDER BY DATE(f.date);
 """
+
+salmon = pd.DataFrame(plpy.execute(salmon_query))
 
 level_query = """
 SELECT DATE(MIN(f.date)) AS date, f.watershed, f.site, f.species, COUNT(DISTINCT f.tag_id_long) AS count
@@ -38,6 +42,8 @@ GROUP BY f.watershed, f.site, f.species, DATE(f.date)
 ORDER BY DATE(f.date);
 """
 
+level = pd.DataFrame(plpy.execute(level_query))
+
 flow_query = """
 SELECT DATE(MIN(f.date)) AS date, f.watershed, f.site, f.species, COUNT(DISTINCT f.tag_id_long) AS count
 FROM FIELD f
@@ -50,35 +56,42 @@ GROUP BY f.watershed, f.site, f.species, DATE(f.date)
 ORDER BY DATE(f.date);
 """
 
+flow = pd.DataFrame(plpy.execute(flow_query))
 
 
+def preprocess_sql(salmon, cowichan_historic):
+    # Filter and reset index for Cowichan watershed
+    salmon_cow_temp = salmon[salmon['watershed'] == 'cowichan'].reset_index()
+    salmon_cow = salmon_cow_temp[['date', 'species', 'site', 'count']]
+    salmon_cow = salmon_cow[salmon_cow['species'].isin(['co', 'ck'])]
 
+    # Create dummy variables for sites
+    site_dummies = pd.get_dummies(salmon_cow['site'])
+    df_expanded = pd.concat([salmon_cow, site_dummies], axis=1)
+    df_expanded = df_expanded.drop('site', axis=1)
 
+    # Standardize species names and concatenate datasets
+    cowichan_historic['species'] = cowichan_historic['species'].str.lower()
+    df_long = pd.concat([cowichan_historic, df_expanded]).reset_index(drop=True)
+    df_long['species'] = df_long['species'].replace('cn', 'ck')
 
+    # Filter and create dummy variables for species
+    df_long_filter = df_long[df_long['species'].isin(['co', 'ck'])]
+    species_dummies = pd.get_dummies(df_long_filter['species'])
+    df_expanded_1 = pd.concat([df_long_filter, species_dummies], axis=1)
+    df_expanded_1 = df_expanded_1.drop('species', axis=1)
 
+    return df_expanded_1
+    
+salmon_concat = preprocess_sql(salmon, cowichan_historic)
 
-
-
-
-
-
-
-
-
-
-
-
-def preprocessing(species, salmon_df_name, temp_df_name, level_df_name, flow_df_name):
-    df_salmon = pd.read_csv(salmon_df_name)
+def preprocessing(species, df_salmon, df_temp, df_level, df_flow):
     df_salmon = df_salmon[df_salmon[species] == True][["date", "count"]].groupby("date").sum().reset_index()
 
-    df_temp = pd.read_csv(temp_df_name)
-
-    df_files = [("Level", "LEVEL", level_df_name), ("Flow", "FLOW", flow_df_name)]
+    df_files = [("Level", "LEVEL", df_level), ("Flow", "FLOW", df_flow)]
 
     pivoted_dfs = {}
-    for value_name, col_prefix, file_name in df_files:
-        df = pd.read_csv(file_name)
+    for value_name, col_prefix, df in df_files:
         df_pivoted = df.melt(id_vars=["STATION_NUMBER", "YEAR", "MONTH"], var_name="Day", value_name=value_name)
         df_pivoted["Day"] = df_pivoted["Day"].str.replace(col_prefix, "").astype(int)
         df_pivoted["Date"] = pd.to_datetime(df_pivoted["YEAR"].astype(str) + "-" + df_pivoted["MONTH"].astype(str) + "-" + df_pivoted["Day"].astype(str), errors='coerce')
@@ -86,7 +99,7 @@ def preprocessing(species, salmon_df_name, temp_df_name, level_df_name, flow_df_
         df_pivoted[value_name] = pd.to_numeric(df_pivoted[value_name], errors='coerce')
         numeric_columns = df_pivoted.select_dtypes(include=[np.number]).columns
         pivoted_dfs[value_name] = df_pivoted.groupby("Date")[numeric_columns].mean().reset_index()
-
+ 
     comb = df_salmon.merge(df_temp[["UTC_DATE", "TEMP"]], left_on="date", right_on="UTC_DATE", how="right").drop("date", axis=1).fillna(0).rename(columns={"UTC_DATE": "date"})
     comb["date"] = pd.to_datetime(comb["date"])
     comb["month"], comb["year"] = comb["date"].dt.month, comb["date"].dt.year
@@ -119,11 +132,4 @@ def preprocessing(species, salmon_df_name, temp_df_name, level_df_name, flow_df_
     comb_df.fillna(comb_df.median(), inplace=True)
     return comb_df
 
-if __name__ == "__main__":
-    salmon_path = "../data/salmon_concat.csv"
-    temp_path = "../data/northcochiwan_daily_temp-2.csv"
-    flow_path = "../data/flow_2023.csv"
-    level_path = "../data/level_2023.csv"
-
-    final_df = preprocessing("ck", salmon_path, temp_path, level_path, flow_path)
-    final_df.to_csv("../data/jenny/preprocessed_ck.csv", index=False)
+preprocessed = preprocessing("ck", salmon_concat, ,level, flow)
